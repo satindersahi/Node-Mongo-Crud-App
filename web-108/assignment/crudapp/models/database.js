7c168079e367ed0103676ca6daea1433617dfc1d
satindersahi <62856133+satindersahi@users.noreply.github.com>
2021-04-28 21:53:05 -0400

    Update database.js

--- web-108/assignment/crudapp/models/database.js
+++ web-108/assignment/crudapp/models/database.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://sp:sp3056@localhost:27017/EmployeesDB', {useNewUrlParser: true}, (err)=>{
+mongoose.connect('', {useNewUrlParser: true}, (err)=>{
   if (!err) {console.log ('MongoDB Connection Succeeded.') }
   else { console.log('error in DB connection : ' + err)}
 });
